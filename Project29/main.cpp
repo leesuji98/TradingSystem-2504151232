f05e4f63398da2457b2a7393e04b2a9356dd3a90
kdh <dongri.kang@sansumg.com>
2025-04-15 14:43:58 +0900

    temp

--- Project29/main.cpp
+++ Project29/main.cpp
@@ -1,7 +1,68 @@
 #include "gmock/gmock.h"
 
+enum Stock
+{
+	Stock_Kiwer,
+	Stock_Nemo,
+};
 
-int main() {
+int main()
+{
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
 }
+
+class StockBroker		
+{
+public:
+	void SelectStockDriver(Stock eStock)
+	{
+		switch(eStock)
+		{
+		case Stock_Kiwer:
+			pstDriver = KiwerDriver::GetInstance();
+			break;
+		case Stock_Nemo:
+			pstDriver = NemoDriver::GetInstance();
+			break;
+		}
+	}
+
+	void buy()
+	{
+		pstDriver->buy();
+	}
+private:
+	IDriver *pstDriver;
+};
+
+class IDriver
+{
+public:
+	virtual bool login(std::string id, std::string password) = 0;
+	virtual void buy() = 0;
+    virtual void sell() = 0;
+    virtual void selectStockBrocker(string name) = 0;
+};
+
+class NemoDriver : public IDriver
+{
+public:
+	void buy()
+	{
+		stNemoApi.sellingStock();
+	}
+private:
+	NemoAPI stNemoAPI;
+};
+
+class KiwerDriver : public IDriver
+{
+public:
+	void buy()
+	{
+		stNemoApi.sellingStock();
+	}
+private:
+	KiwerAPI stNemoAPI;
+};
