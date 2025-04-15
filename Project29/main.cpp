e4ba05d7822c67b2e9b2c358fef71414fb5154ca
leesuji <leesuji98@naver.com>
2025-04-15 14:16:37 +0900

    TC1 : ChoiceBrocker, LoginSuccess

--- Project29/main.cpp
+++ Project29/main.cpp
@@ -1,5 +1,36 @@
+#include <string>
 #include "gmock/gmock.h"
+#include "gtest/gtest.h"
 
+using std::string;
+using namespace testing;
+
+class StockBrocker {
+public:
+	virtual bool login(std::string id, std::string password) = 0;
+	virtual void selectStockBrocker(string name) = 0;
+};
+
+class MockStockBroker : public StockBrocker {
+public:
+	MockStockBroker() {}
+	MOCK_METHOD(void, selectStockBrocker, (string name), (override));
+	MOCK_METHOD(bool, login, (string id, string password), (override));
+};
+
+TEST(StockBrockerTest, ChoiceBrocker) {
+	MockStockBroker mock;
+//	EXPECT_CALL(mock, selectStockBrocker("K")).Times(1).WillOnce();
+
+	mock.selectStockBrocker("K");
+}
+
+TEST(StockBrockerTest, LoginSuccess) {
+	MockStockBroker mock;
+	EXPECT_CALL(mock, login("user", "pass")).WillOnce(Return(true));
+
+	EXPECT_TRUE(mock.login("user", "pass"));
+}
 
 int main() {
 	::testing::InitGoogleMock();
